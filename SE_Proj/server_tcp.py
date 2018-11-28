c6dc6f75fe808bade047e227e8df6a8de6571a06
Pranav <pranavvardia@gmail.com>
2018-11-28 22:01:52 +0530

    write out on file

--- SE_Proj/server_tcp.py
+++ SE_Proj/server_tcp.py
@@ -37,7 +37,8 @@ while True:
 	priv_key = RSA.importKey(open('private.pem').read())
 	cipher1 = PKCS1_OAEP.new(priv_key)
 	final = cipher1.decrypt(msg)
-	print(final.decode('utf-8'))
+	with open('output.txt', 'w') as f:
+		f.write(final.decode('utf-8'))
 
 conn.close()
 server.close()
